c9097e351f5d42fa448371344f0114f97d32e061
tessam30 <tim.essam@gmail.com>
2020-08-28 10:35:03 -0400

    update info

--- Scripts/KEN_netnew.R
+++ Scripts/KEN_netnew.R
@@ -23,8 +23,8 @@ source(file.path("Scripts", "setup.R"))
 # API ---------------------------------------------------------------------
 
   #DATIM Information
-    myuser <- "tessam"
-    yawn <- decrypt_kc_pw("mydb_access")
+    myuser <- ""
+    yawn <- ""
     baseurl <- "https://final.datim.org/"
     
     
@@ -115,18 +115,21 @@ df_genie <- read_rds(file.path(datain, "MER_Structured_Dataset_GENIESITE_IM_FY18
         
         return(df_site_tx_reassigned)
     }
+   
     
-  prinf <- function(df) {
-    print(df, n = Inf)
-  }   
-
-# number of rows for facet
-  facet_rows <- function(df) { 
-    df %>% 
-    distinct(fundingagency) %>% 
-    count() %>% 
-    pull()
-  }
+    
+   # print to infinity with prinf  
+    prinf <- function(df) {
+      print(df, n = Inf)
+    }   
+  
+  # number of rows for facet
+    facet_rows <- function(df, var = fundingagency) { 
+      df %>% 
+      distinct({{var}}) %>% 
+      count() %>% 
+      pull()
+    }
   
   
 # MUNGE -------------------------------------------------------------------
@@ -226,15 +229,64 @@ df_genie <- read_rds(file.path(datain, "MER_Structured_Dataset_GENIESITE_IM_FY18
       group_by(operatingunit, orgunituid) %>% 
       mutate(orgunituid_shift = if_else(agency_shift == 1, 1, NA_real_)) %>% 
       fill(orgunituid_shift, .direction = c("up")) %>%
-      fill(agency_shift_type, .direction = c("up")) %>% 
+      fill(agency_shift_type, .direction = c("up")) %>%
       ungroup()
     
-    df_site_tx_clean <- 
+    df_site_tx_clean_mm <- 
       df_site_tx_clean %>% 
       filter(!exclude_id %in% mp_site_exclude) %>% 
       # Collapsing on occurence where an IP should have been given credit for 36 more (check on this>?!)
       mutate(tx_curr = if_else(exclude_id == "y1337MIdqZGUSAID14012FY18Q1179", tx_curr + 36, tx_curr)) %>% 
-      filter(exclude_id != "y1337MIdqZGUSAID14022FY18Q136")
+      filter(exclude_id != "y1337MIdqZGUSAID14022FY18Q136") %>% 
+      # Flag mechanisms who drop in and out of time series; This seems to affecting net new calculation
+      arrange(orgunituid, period) %>% 
+      group_by(orgunituid) %>% 
+      add_tally(name = "period_count") %>% 
+      #mutate(tx_net_new = tx_curr - lag(tx_curr, order_by = period)) %>% 
+      ungroup()
+    
+  # Summary of the different cases of sites - spells + agency shifts
+    df_site_tx_clean_mm %>% count(period_count, agency_shift, sort = TRUE)
+    df_site_tx_clean_mm %>% filter(period_count < 10 & !is.na(agency_shift_type)) %>% view()
+    
+  # Depending on what grouping is used, you may mis-count or drop sites due to the grouping structure and the spell length.
+  # If you aggregate first at the agency level, the spell length doesn't matter. It's all USAID tx_curr added for a period
+  # minus the lag of all the aggregated tx_curr from the last period (regardless of spell length).
+  # Case1. Sites that have complete runs (period_count == 10 and no agency_shift == 0) - ok
+  # Case 2. Sites that have incomplete runs (period_count < 10 and no agency_shift == 0) --> can safely aggregate to agency
+  # Case 3. Sites that have incomoplete runs & agency shifts (period_count < 10 & agency_shift == 1)
+  # Or. just ignore everything and apply aggregation with limitations
+  
+#  tx_nn_agency <- df_site_tx_clean_mm %>% 
+#    filter(period_count == 10) %>% 
+#    arrange(orgunituid, period) %>% 
+#    group_by(orgunituid) %>% 
+#    mutate(tx_net_new = tx_curr - lag(tx_curr, order_by = period)) %>% 
+#    ungroup() %>% 
+#   filter(period != "FY17Q4")
+#  
+#  tx_nn_agency %>% group_by(fundingagency, period) %>% summarise(txnn = sum(tx_net_new, na.rm = TRUE))
+# 
+#  tx_nn_noshift <- df_site_tx_clean_mm %>% 
+#    filter(period_count != 10 & agency_shift == 0) %>%
+#    arrange(fundingagency, period) %>% 
+#    group_by(fundingagency) %>% 
+#    mutate(tx_net_new = tx_curr - lag(tx_curr, order_by = period)) %>% 
+#    ungroup() %>% 
+#   filter(period != "FY17Q4")
+#  
+# tx_nn_noshift %>% group_by(fundingagency, period) %>% 
+#   summarise(txnn = sum(tx_net_new, na.rm = TRUE))
+# 
+# df_site_tx_clean_mm %>% 
+#    filter(period_count != 10 & agency_shift == 0) %>% group_by(fundingagency, period) %>% 
+#   summarise(txnn = sum(tx_net_new, na.rm = TRUE))
+#  
+# tx_nn_shifts <- df_site_tx_clean_mm %>% 
+#   filter(period_count != 10 & agency_shift == 1)
+   
+
+
     
   # Show a summary of sites that switched from USAID to CDC and vice versa
   df_site_tx_clean %>% 
@@ -266,7 +318,7 @@ df_targets <-
 df_site_tx_tgts <- df_site_tx_clean %>%
   left_join(., df_targets, by = c("orgunituid", "mech_code", "period")) 
 
-
+write_csv(df_site_tx_clean, file.path(dataout, "tmp.csv"))
 
   
 # SITE TRANSITION PLOTS ---------------------------------------------------
@@ -292,8 +344,12 @@ df_site_tx_tgts <- df_site_tx_clean %>%
       legend.justification = c(0, 0),
       legend.direction = "horizontal") +
     labs(x = NULL, y = NULL, 
-    title = "Summary of site transitions at the county level")
-    
+    title = "Summary of site transitions at the county level") 
+ 
+  ggsave(file.path(graphs, "KEN_site_transition_summary_by_county.png"),
+    last_plot(), scale = 1.4,
+     dpi = 330, width = 7, height = 4)  
+  
   
   df_site_tx_clean %>% 
     filter(orgunituid_shift == 1) %>% 
@@ -306,7 +362,7 @@ df_site_tx_tgts <- df_site_tx_clean %>%
       ) %>% 
     ggplot(aes(x = period, y = site_order, fill = fundingagency)) +
     geom_tile(colour = "white")  +
-    ggforce::facet_row(~snu1_order, scales = "free_x", space = "free",
+    facet_wrap(~snu1_order, scales = "free_y",
     labeller = label_wrap_gen(multi_line = FALSE)) +
     #facet_wrap(~snu1_order, ncol = 13) +
     scale_fill_manual(values = c("#335B8E", "#6CA18F")) +
@@ -325,12 +381,10 @@ df_site_tx_tgts <- df_site_tx_clean %>%
       caption = paste0("DATIM Genie API Pull [", format(Sys.Date(), "%Y-%m-%d"), "]"),
       fill = NULL)
   
-    ggsave(file.path(graphs, "KEN_site_transition_summary_by_county.pdf"),
+    ggsave(file.path(graphs, "KEN_site_transition_summary_by_county_SM.png"),
       plot = last_plot(),
       width = 23.4,
       height = 16.5,
-      device = "pdf",
-      useDingbats = FALSE, 
       units = "in"
       )
         
@@ -378,10 +432,10 @@ df_site_tx_tgts <- df_site_tx_clean %>%
     }
     
     
-    plot_tx_ip <- function(df, var, title){
+    plot_tx_snu <- function(df, var, title){
       
-      if("primepartner_adj" %in% names(df))
-        df <- mutate(df, primepartner = primepartner_adj)
+      # if("primepartner_adj" %in% names(df))
+      #   df <- mutate(df, primepartner = primepartner_adj)
       
       df %>% 
         mutate(placeholder = {{var}} * 1.4) %>% 
@@ -398,7 +452,7 @@ df_site_tx_tgts <- df_site_tx_clean %>%
           "FY19Q1", "", "FY19Q3", "",
           "FY20Q1")) +
         scale_fill_manual(values = c("#335B8E", "#6CA18F")) +
-        facet_grid(primepartner ~ ., switch = "y") +
+        facet_wrap(fundingagency ~ snu1, nrow = 7) +
         labs(x = NULL, y = NULL, 
           title = title,
           caption = paste0("DATIM Genie API Pull [", format(Sys.Date(), "%Y-%m-%d"), "]")) +
@@ -437,14 +491,14 @@ df_site_tx_tgts <- df_site_tx_clean %>%
           "FY20Q1")) +
         scale_color_manual(values = c("#335B8E", "#6CA18F")) +
         scale_fill_manual(values = c("#335B8E", "#6CA18F")) +
-        facet_grid(fundingagency ~ ., switch = "y") +
-        # facet_wrap(~fundingagency, nrow = nrows)
+       #facet_wrap(~fundingagency, ) +
+        facet_wrap(~fundingagency, nrow = nrows) +
         labs(x = NULL, y = NULL, 
           title = title,
           caption = paste0("DATIM Genie API Pull [", format(Sys.Date(), "%Y-%m-%d"), "]")) +
         theme_minimal() +
         theme(text = element_text(family = "Franklin Gothic Medium Cond"),
-          strip.text = element_text(size = 14), #, hjust = 0),
+          strip.text = element_text(size = 14, hjust = 0),
           axis.text.y = element_blank(),
           panel.grid.major.x = element_blank(),
           plot.title = element_text(family = "Franklin Gothic Medium"),
@@ -452,90 +506,107 @@ df_site_tx_tgts <- df_site_tx_clean %>%
           legend.position = "none")
     }
     
+    # County level function to compare CDC vs USAID for net new
+      plot_snu <- function(df, snu = "Turkana") {
+        
+        max <- df %>% summarise(max = max(abs(tx_net_new))) %>% pull()
+        
+        df %>% 
+        filter(snu1 %in% snu) %>% 
+        ggplot(aes(x = period, y = tx_net_new, fill = fundingagency)) +
+      geom_col(position = position_dodge2(preserve = "single")) + 
+      #facet_wrap(~county_sort, scales = "free_y") +
+      ggthemes::theme_tufte() +
+       scale_fill_manual(values = c("#335B8E", "#6CA18F")) +
+      theme(strip.text = element_text(size = 8, hjust = 0),
+        axis.text = element_text(size = 8)) +
+      scale_y_continuous(label = comma,
+        limits = c(-max, max)) +
+        scale_x_discrete(labels = c("FY18Q1", "", "FY18Q3", "",
+          "FY19Q1", "", "FY19Q3", "",
+          "FY20Q1")) +
+          labs(x = "", y = "")
+      }
     
     
     # PLOT TX TRENDS ----------------------------------------------------------
-    # Stopped here
-    
-    
-    
+
     #AGENCY UNADJUSTED
-    df_site_tx_clean %>% 
+    df_site_tx_clean_mm %>% 
       #filter(multi_site_flag != 1) %>% 
       agg_by(operatingunit, fundingagency) %>% 
       plot_tx(tx_curr, "TX_CURR, unadjusted")
     
-    ggsave("out/plots/TZA_NNAdj_TXCURR.png",
+    ggsave("Graphics/KEN_NNAdj_TXCURR.png",
       dpi = 330, width = 7, height = 3.6)
     
-    df_site_tx_clean %>% 
+    df_site_tx_clean_mm %>% 
       #filter(multi_site_flag != 1) %>% 
       agg_by(operatingunit, fundingagency) %>% 
       plot_tx(tx_net_new, "TX_NET_NEW, unadjusted") 
     
-    ggsave("out/plots/TZA_NNAdj_NETNEW.png",
-      dpi = 330, width = 7, height = 3.6)
+    ggsave("Graphics/KEN_NNAdj_NETNEW.png",
+      dpi = 330, width = 7, height = 3.6, scale = 1.4)
     
     
-    df_site_tx_clean %>%
+    df_site_tx_clean_mm %>%
       #filter(multi_site_flag != 1) %>% 
       agg_by(operatingunit, fundingagency) %>%
       plot_nn_gr("Growth in TX_NET_NEW, unadjusted")
     
-    ggsave("out/plots/TZA_NNAdj_NETNEW_gr.png",
-      dpi = 330, width = 7, height = 3.6)
+    ggsave("Graphics/KEN_NNAdj_NETNEW_gr.png",
+      dpi = 330, width = 7, height = 3.6, scale = 1.4)
     
     #AGENCY REASSIGNED
-    df_site_tx_clean %>% 
+    df_site_tx_clean_mm %>% 
       #filter(multi_site_flag != 1) %>%  
       agg_reassign_by(operatingunit, fundingagency) %>% 
       plot_tx(tx_net_new, "TX_NET_NEW, reassigned")
     
-    ggsave("out/plots/TZA_NNAdj_NETNEWADJ.png",
-      dpi = 330, width = 7, height = 3.6)
+    ggsave("Graphics/KEN_NNAdj_NETNEWADJ.png",
+      dpi = 330, width = 7, height = 3.6, scale = 1.4)
     
-    df_site_tx_clean %>% 
+    df_site_tx_clean_mm %>% 
       #filter(multi_site_flag != 1) %>% 
       agg_reassign_by(operatingunit, fundingagency) %>% 
       plot_nn_gr("Growth in TX_NET_NEW, reassigned")
     
-    ggsave("out/plots/TZA_NNAdj_NETNEWADJ_gr.png",
-      dpi = 330, width = 7, height = 3.6)
+    ggsave("Graphics/KEN_NNAdj_NETNEWADJ_gr.png",
+      dpi = 330, width = 7, height = 3.6, scale = 1.4)
     
-
-# TX NN BY PARTNER --------------------------------------------------------
+    
+    # TX NN BY PARTNER --------------------------------------------------------
 
 
   #PARTNER UNADJUSTED
-    df_site_tx_clean %>% 
+# snulist <- c("Kericho", "Wajir", "Mandera", "Tana River", 
+#         "Garissa", "Marsabit", "Samburu", "Lamu", "Isiolo", "Nandi", "West Pokot", "Elgeyo-Marakwet",
+#         "Baringo", "Narok", "Taita Taveta", "Tharaka Nithi", "Turkana", "Nyandarua",
+#         "Laikipia", "Embu", "Kwale", "Kirinyaga", "Kajiado", "Nyamira", "Trans-Nzoia") 
+    
+    df_site_tx_clean_mm %>% 
       #filter(multi_site_flag != 1) %>% 
-      agg_by(operatingunit, fundingagency, primepartner) %>% 
-      filter(primepartner != "THPS") %>% 
-      plot_tx_ip(tx_curr, "TX_CURR, unadjusted")
+      agg_by(operatingunit, fundingagency, snu1) %>%
+      # ungroup() %>% 
+      # group_by(snu1) %>% 
+      # mutate(sortvar = sum(tx_curr, na.rm = TRUE)) %>% 
+      # ungroup() %>% 
+      #mutate(ymax = ifelse(!snu1 %in% snulist, 100000, 12500),
+      #  ymin = 0) %>% 
+      #mutate(county_sort = fct_reorder(snu1, sortvar, .desc = TRUE)) %>% 
+      plot_snu(snu = "Nairobi")
+      
+
+  #PARTNER REASSIGNED
+    df_site_tx_clean_mm %>%
+      filter(period_count == 10) %>% 
+      agg_reassign_by(operatingunit, fundingagency, snu1) %>% 
+         plot_snu(snu = c("Nairobi", "Turkana")) +
+      facet_wrap(~snu1)
     
-    ggsave("out/plots/TZA_NNAdj_TXCURR_IM.png",
-      dpi = 330, width = 7, height = 7.11)
-    
-    df_site_tx_clean %>% 
-      filter(multi_site_flag != 1) %>% 
-      agg_by(operatingunit, fundingagency, primepartner) %>% 
-      filter(primepartner != "THPS") %>% 
-      plot_tx_ip(tx_net_new, "TX_NET_NEW, unadjusted")
-    
-    ggsave("out/plots/TZA_NNAdj_NETNEW_IM.png",
-      dpi = 330, width = 7, height = 7.11)
-    
-    
-    
-    #PARTNER REASSIGNED
-    df_site_tx_clean %>% 
-      filter(multi_site_flag != 1) %>% 
-      agg_reassign_by(operatingunit, fundingagency, primepartner) %>% 
-      filter(primepartner != "THPS") %>% 
-      plot_tx_ip(tx_net_new, "TX_NET_NEW, reassigned")
-    
-    ggsave("out/plots/TZA_NNAdj_NETNEWADJ_IM.png",
-      dpi = 330, width = 7, height = 7.11)
+      
+      
+
     
     
     
